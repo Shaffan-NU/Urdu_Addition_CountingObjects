20a0bc16c8d71f61e524d81e959a0378c8a11aed
shaffanFast <k190863@nu.edu.pk>
2021-10-04 11:53:51 +0500

    Updates

---
+++ precache-manifest.3877ac9118f1544a769a93a4fcb8a1a1.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d43a5c09b4bd090208e",
+    "url": "/Urdu_Addition_CountingObjects/static/css/main.82a87590.chunk.css"
+  },
+  {
+    "revision": "6d43a5c09b4bd090208e",
+    "url": "/Urdu_Addition_CountingObjects/static/js/main.1e74b8d9.chunk.js"
+  },
+  {
+    "revision": "b4e5506c4b34234dd76f",
+    "url": "/Urdu_Addition_CountingObjects/static/js/runtime~main.bc3f6616.js"
+  },
+  {
+    "revision": "893e04138f7175cf0ded",
+    "url": "/Urdu_Addition_CountingObjects/static/css/2.96d79ed8.chunk.css"
+  },
+  {
+    "revision": "893e04138f7175cf0ded",
+    "url": "/Urdu_Addition_CountingObjects/static/js/2.1f93acba.chunk.js"
+  },
+  {
+    "revision": "f0fad205a64bf114ea96",
+    "url": "/Urdu_Addition_CountingObjects/static/js/3.1adaacc5.chunk.js"
+  },
+  {
+    "revision": "40d8e09c1bc1e01df2f2d0350718c089",
+    "url": "/Urdu_Addition_CountingObjects/static/media/_1.40d8e09c.mp3"
+  },
+  {
+    "revision": "65a6b97dc7801719b2d140c796276265",
+    "url": "/Urdu_Addition_CountingObjects/static/media/_2.65a6b97d.mp3"
+  },
+  {
+    "revision": "6bd419110b7fb9b6b44fd2f477a35abc",
+    "url": "/Urdu_Addition_CountingObjects/static/media/_3.6bd41911.mp3"
+  },
+  {
+    "revision": "ae5c58d75da67259d8f8a689eb4040cd",
+    "url": "/Urdu_Addition_CountingObjects/static/media/_4.ae5c58d7.mp3"
+  },
+  {
+    "revision": "fa32eb88ca708bfdd1f506d0d6ad7163",
+    "url": "/Urdu_Addition_CountingObjects/static/media/_5.fa32eb88.mp3"
+  },
+  {
+    "revision": "639f363f9c8589d7472737d7d86ca22b",
+    "url": "/Urdu_Addition_CountingObjects/static/media/_6.639f363f.mp3"
+  },
+  {
+    "revision": "7fc0455545ca15ac611aabfd2aa9d5fc",
+    "url": "/Urdu_Addition_CountingObjects/static/media/_7.7fc04555.mp3"
+  },
+  {
+    "revision": "9c9b9bcb08b586e22914148c5f69bcd6",
+    "url": "/Urdu_Addition_CountingObjects/static/media/_8.9c9b9bcb.mp3"
+  },
+  {
+    "revision": "fc30493cbaf5493f1204486e9e9cbbf1",
+    "url": "/Urdu_Addition_CountingObjects/static/media/_9.fc30493c.mp3"
+  },
+  {
+    "revision": "594c0b0ed7da5faa738c12be102b3018",
+    "url": "/Urdu_Addition_CountingObjects/static/media/_10.594c0b0e.mp3"
+  },
+  {
+    "revision": "91412d063f2fde22edf3a015fa63a786",
+    "url": "/Urdu_Addition_CountingObjects/static/media/removeItem.91412d06.mp3"
+  },
+  {
+    "revision": "74562e5d9b36cc740a89848ba488bf7d",
+    "url": "/Urdu_Addition_CountingObjects/static/media/rooster.74562e5d.png"
+  },
+  {
+    "revision": "cabc22c6e80bc043c10b098b6b8854bb",
+    "url": "/Urdu_Addition_CountingObjects/static/media/bowl.cabc22c6.png"
+  },
+  {
+    "revision": "c040d63c530ff46d273f95822ecb12e7",
+    "url": "/Urdu_Addition_CountingObjects/static/media/football10.c040d63c.png"
+  },
+  {
+    "revision": "c989b2046b4fa39573d918b056723605",
+    "url": "/Urdu_Addition_CountingObjects/static/media/football1.c989b204.png"
+  },
+  {
+    "revision": "fe892d45a0838c6408beabefe99b4aa6",
+    "url": "/Urdu_Addition_CountingObjects/static/media/bouncingFootball.fe892d45.wav"
+  },
+  {
+    "revision": "f5f258693cd37ccdf82ff06568ee9082",
+    "url": "/Urdu_Addition_CountingObjects/static/media/background.f5f25869.gif"
+  },
+  {
+    "revision": "b512cd285a5ca16e6e3ec0c535047a76",
+    "url": "/Urdu_Addition_CountingObjects/static/media/background2.b512cd28.gif"
+  },
+  {
+    "revision": "aba6e8649f32d040300aa0f7007b69d9",
+    "url": "/Urdu_Addition_CountingObjects/index.html"
+  }
+];
