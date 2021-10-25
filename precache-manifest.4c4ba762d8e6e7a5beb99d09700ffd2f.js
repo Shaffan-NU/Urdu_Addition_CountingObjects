a16c77cb255b85d66a00c05da549ac856a0759ee
shaffanFast <k190863@nu.edu.pk>
2021-10-25 12:14:12 +0500

    Updates

---
+++ precache-manifest.4c4ba762d8e6e7a5beb99d09700ffd2f.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a85ff749c27e61f8b14",
+    "url": "/Urdu_Addition_CountingObjects/static/css/main.6a32a235.chunk.css"
+  },
+  {
+    "revision": "5a85ff749c27e61f8b14",
+    "url": "/Urdu_Addition_CountingObjects/static/js/main.adfb34a0.chunk.js"
+  },
+  {
+    "revision": "4bca5c6e001f4a612895",
+    "url": "/Urdu_Addition_CountingObjects/static/js/runtime~main.12d9cdda.js"
+  },
+  {
+    "revision": "cee87a316d975e8983b7",
+    "url": "/Urdu_Addition_CountingObjects/static/css/2.96d79ed8.chunk.css"
+  },
+  {
+    "revision": "cee87a316d975e8983b7",
+    "url": "/Urdu_Addition_CountingObjects/static/js/2.b04a5663.chunk.js"
+  },
+  {
+    "revision": "1ddffff5f4ae930e9f2f",
+    "url": "/Urdu_Addition_CountingObjects/static/js/3.e97662f7.chunk.js"
+  },
+  {
+    "revision": "91412d063f2fde22edf3a015fa63a786",
+    "url": "/Urdu_Addition_CountingObjects/static/media/removeItem.91412d06.mp3"
+  },
+  {
+    "revision": "2fd9ac2208c833754484473decfc84a1",
+    "url": "/Urdu_Addition_CountingObjects/static/media/drop.2fd9ac22.wav"
+  },
+  {
+    "revision": "57a2639bd6d80374aaf0714a48200f92",
+    "url": "/Urdu_Addition_CountingObjects/static/media/10.57a2639b.svg"
+  },
+  {
+    "revision": "a7f0e4e3c9b56642615788ea350047d6",
+    "url": "/Urdu_Addition_CountingObjects/static/media/1.a7f0e4e3.svg"
+  },
+  {
+    "revision": "7360a269fa89ec8922a9fb9354820523",
+    "url": "/Urdu_Addition_CountingObjects/static/media/sound.7360a269.png"
+  },
+  {
+    "revision": "1e695820a1572347f6a0a76f53280775",
+    "url": "/Urdu_Addition_CountingObjects/static/media/tostartpressbutton.1e695820.mp3"
+  },
+  {
+    "revision": "b8fca396c389eea881872cc7f0e7855b",
+    "url": "/Urdu_Addition_CountingObjects/static/media/321go.b8fca396.mp3"
+  },
+  {
+    "revision": "88e8a851b1954501951ae45cf46c1e2f",
+    "url": "/Urdu_Addition_CountingObjects/static/media/rightAnswer.88e8a851.mp3"
+  },
+  {
+    "revision": "48b32efb6257ce0ee9f40b87b5894871",
+    "url": "/Urdu_Addition_CountingObjects/static/media/wrongAnswer.48b32efb.mp3"
+  },
+  {
+    "revision": "cabc22c6e80bc043c10b098b6b8854bb",
+    "url": "/Urdu_Addition_CountingObjects/static/media/bowl.cabc22c6.png"
+  },
+  {
+    "revision": "74562e5d9b36cc740a89848ba488bf7d",
+    "url": "/Urdu_Addition_CountingObjects/static/media/rooster.74562e5d.png"
+  },
+  {
+    "revision": "40d8e09c1bc1e01df2f2d0350718c089",
+    "url": "/Urdu_Addition_CountingObjects/static/media/_1.40d8e09c.mp3"
+  },
+  {
+    "revision": "65a6b97dc7801719b2d140c796276265",
+    "url": "/Urdu_Addition_CountingObjects/static/media/_2.65a6b97d.mp3"
+  },
+  {
+    "revision": "6bd419110b7fb9b6b44fd2f477a35abc",
+    "url": "/Urdu_Addition_CountingObjects/static/media/_3.6bd41911.mp3"
+  },
+  {
+    "revision": "ae5c58d75da67259d8f8a689eb4040cd",
+    "url": "/Urdu_Addition_CountingObjects/static/media/_4.ae5c58d7.mp3"
+  },
+  {
+    "revision": "fa32eb88ca708bfdd1f506d0d6ad7163",
+    "url": "/Urdu_Addition_CountingObjects/static/media/_5.fa32eb88.mp3"
+  },
+  {
+    "revision": "639f363f9c8589d7472737d7d86ca22b",
+    "url": "/Urdu_Addition_CountingObjects/static/media/_6.639f363f.mp3"
+  },
+  {
+    "revision": "7fc0455545ca15ac611aabfd2aa9d5fc",
+    "url": "/Urdu_Addition_CountingObjects/static/media/_7.7fc04555.mp3"
+  },
+  {
+    "revision": "9c9b9bcb08b586e22914148c5f69bcd6",
+    "url": "/Urdu_Addition_CountingObjects/static/media/_8.9c9b9bcb.mp3"
+  },
+  {
+    "revision": "fc30493cbaf5493f1204486e9e9cbbf1",
+    "url": "/Urdu_Addition_CountingObjects/static/media/_9.fc30493c.mp3"
+  },
+  {
+    "revision": "594c0b0ed7da5faa738c12be102b3018",
+    "url": "/Urdu_Addition_CountingObjects/static/media/_10.594c0b0e.mp3"
+  },
+  {
+    "revision": "8164e48c115fae4bb874ed0c63b40cbc",
+    "url": "/Urdu_Addition_CountingObjects/static/media/instructions.8164e48c.gif"
+  },
+  {
+    "revision": "d6687daa9e8311c3106547e339b95f38",
+    "url": "/Urdu_Addition_CountingObjects/static/media/background.d6687daa.gif"
+  },
+  {
+    "revision": "b512cd285a5ca16e6e3ec0c535047a76",
+    "url": "/Urdu_Addition_CountingObjects/static/media/background2.b512cd28.gif"
+  },
+  {
+    "revision": "a0d6b13874a29393b0f8252f2adb5fa7",
+    "url": "/Urdu_Addition_CountingObjects/static/media/grocerySound.a0d6b138.mp3"
+  },
+  {
+    "revision": "f02ac89a44b28acd80cb6b0399adbf8f",
+    "url": "/Urdu_Addition_CountingObjects/static/media/mute.f02ac89a.png"
+  },
+  {
+    "revision": "a4428eff42d8c4ecc3f96656a7f05d22",
+    "url": "/Urdu_Addition_CountingObjects/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Urdu_Addition_CountingObjects/precache-manifest.dc74828a61a0ca65963ec3169973849f.js"
+  "/Urdu_Addition_CountingObjects/precache-manifest.4c4ba762d8e6e7a5beb99d09700ffd2f.js"
 );
 
 workbox.clientsClaim();
